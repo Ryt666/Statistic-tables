30f84cbdc013eaf20d905b6ba10cf72e6da5b106
Polina <polinachka0603@gmail.com>
2022-10-04 22:11:22 +0300

    first commit
    out commit

---
+++ project_one/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double[] mas = new double;
+		for(int i = 0; i < 10; i++)
+		{
+		    mas[i] = i;
+		}
+		System.out.print(mas);
+		
+	}
+
+}
